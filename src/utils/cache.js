0c53109469f59e35adec1e7a6c09394e242eabd2
Gunabh Sharan <gunabhsharan.25@gmail.com>
2025-11-16 21:32:35 +0530

    pickup the REDIS_URL from Render's environment variables

--- src/utils/cache.js
+++ src/utils/cache.js
@@ -1,12 +1,13 @@
 const Redis = require('ioredis');
-const { redisUrl } = require('../config');
 const client = new Redis(process.env.REDIS_URL);
 
 module.exports = {
-  get: async (key) => await client.get(key),
-  set: async (key, value, ttlSeconds) => {
-    if (ttlSeconds) return await client.set(key, value, 'EX', ttlSeconds);
-    return await client.set(key, value);
+  get: (key) => client.get(key),
+  set: (key, value, ttlSeconds) => {
+    if (ttlSeconds) {
+      return client.set(key, value, 'EX', ttlSeconds);
+    }
+    return client.set(key, value);
   },
-  del: async (key) => await client.del(key)
+  del: (key) => client.del(key)
 };
