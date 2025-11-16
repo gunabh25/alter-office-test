009cbb211825fa717a9dcf6fbec4d5159b3ffc83
Gunabh Sharan <gunabhsharan.25@gmail.com>
2025-11-16 23:08:14 +0530

    import middleware in app.js

--- src/app.js
+++ src/app.js
@@ -4,10 +4,10 @@ const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const YAML = require('yamljs');
 const swaggerUi = require('swagger-ui-express');
-
 const authRoutes = require('./routes/auth');
 const analyticsRoutes = require('./routes/analytics');
 const errorHandler = require('./middleware/errorHandler');
+const rateLimiter = require("./middleware/rateLimiter");
 
 const app = express();
 
@@ -15,6 +15,7 @@ app.use(helmet());
 app.use(morgan('dev'));
 app.use(bodyParser.json({limit: '1mb'}));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(rateLimiter);
 
 // Swagger
 const swaggerDocument = YAML.load('./openapi.yaml');
